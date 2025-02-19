c4cb0750ad610cade075a89b4eea29d736eb48d9
aiquilibrium <aiquilibrium@proton.me>
2025-02-19 00:14:54 -0500

    about page

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ function fillNavbar(){
 };
 
 function fillFooter(){
-    
+    const footer = document.getElementById("footer");
+    footer.classList.add("text-center");
+    footer.classList.add("mt-4");
+
+ footer.innerHTML = `<div class="footer-links">
+ <a href="#"><i class="fa fa-facebook-official" style="font-size:36px"></i></a>
+ <a href="#"><i class="fa fa-instagram" style="font-size:36px"></i></a>
+ <a href="#"><i class="fa fa-twitter-square" style="font-size:36px"></i></a>
+ </div>
+ <p>Copyright © 2025 Theo Francis. All rights reserved.</p>`; 
 };
 
 
